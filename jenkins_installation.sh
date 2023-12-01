e9a15a71ea40fc4e586ee35397fbd60b59631bed
ankitkumar-devops <119074004+ankitkumar-devops@users.noreply.github.com>
2023-12-01 21:42:32 +0530

    Update jenkins_installation.sh

--- jenkins_installation.sh
+++ jenkins_installation.sh
@@ -6,16 +6,13 @@ sudo apt update &&  apt-get upgrade -y
 sudo apt install maven -y 
 
 # Install java 
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install fontconfig openjdk-17-jre -y
 java --version
 
 # Adding key 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
   
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
   
 # installing jenkins
 sudo apt-get update && apt-get install jenkins -y
